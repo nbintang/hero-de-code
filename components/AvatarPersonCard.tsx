0fd916c0967b631fa42c35c465a19fa04a2444cc
nbintang <nbintangh@gmail.com>
2024-10-22 16:38:41 +0700

    add avatar for team section

--- components/AvatarPersonCard.tsx
+++ components/AvatarPersonCard.tsx
@@ -12,9 +12,9 @@ const AvatarPersonCard = () => {
         <h4 className="text-xl">Dr. John Doe, PhD</h4>
         <p className="text-sm">Board-Certified Psychiatrist</p>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-2 mt-4">
         {[1, 2, 3].map((_, i) => (
-          <div key={i} className="p-2 bg-black rounded-full">
+          <div key={i} className="p-1 bg-black rounded-full">
             <Linkedin className="text-white w-3 h-3" />
           </div>
         ))}
