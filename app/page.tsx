881a98b1475e817a678ba641550503b91162e582
nbintang <nbintangh@gmail.com>
2024-10-22 15:29:24 +0700

    nambahin fragments

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <div className="w-full ">
+ <>
       <div className="bg-primary text-white">Primary</div>
       <div className="bg-secondary text-white">Secondary</div>
       <div className="bg-tertiary text-black">Tertiary</div>
 
       <div className="bg-primary/50 text-white"> primary softed</div>
-    </div>
+      </>
   );
 }
