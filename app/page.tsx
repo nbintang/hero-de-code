13477bdb0e08684eef4d17d64d61424ae40deb0b
nbintang <nbintangh@gmail.com>
2024-10-22 15:59:32 +0700

    header team section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 
 import Hero from "@/components/sections/Hero";
+import Team from "@/components/sections/Team";
 export default function Home() {
   return (
     <>
       <Hero />
+      <Team/>
     </>
   );
 }
---
+++ components/sections/Team.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Team() {
+  return (
+    <section>
+      <h5 className="text-primary text-sm">Meet Our Team</h5>
+      <div className="flex justify-between items-center">
+      <h1 className="text-5xl font-medium flex-[0_0_45%]">
+      Compassionate, Experienced, and Ready to Support You
+    </h1>
+    <p className="text-xl text-black/60 flex-[0_0_40%]">
+      Our team of licensed therapists and mental health professionals bring a wealth of knowledge and empathy to guide you on your wellness journey.
+    </p>
+      </div>
+      <div>tes</div>
+    </section>
+  );
+}
