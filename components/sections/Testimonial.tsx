12a6e606f61bab37993740b553a422949647cd0a
Muhammad Fauzan <m.fauzan.hb@gmail.com>
2024-10-29 19:31:05 +0700

    testimonial

--- components/sections/Testimonial.tsx
+++ components/sections/Testimonial.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Marquee } from "../ui/marquee";
 import TestimonialCard from "../TestimonialCard";
+import dataTestimonial from "@/data/dataTestimonial";
 
-export default function Testimonial() {
+export default async function Testimonial() {
+  const data = await dataTestimonial();
   return (
     <section className="w-full ">
       <div className="max-w-6xl mx-auto mt-5">
@@ -16,18 +18,36 @@ export default function Testimonial() {
         </div>
         <div className="relative flex  my-5 w-full flex-col items-center justify-center overflow-hidden rounded-lg ">
           <Marquee pauseOnHover className="[--duration:20s]">
-            {[1, 2, 3, 4].map((i) => (
-              <TestimonialCard key={i} quote="Some testimonial quote" author="John Doe" role="Patient"/>
+            {data.map((item, i) => (
+              <TestimonialCard
+                key={i}
+                quote={item.quote}
+                author={item.author}
+                role={item.role}
+                avatarSrc={item.img}
+              />
             ))}
           </Marquee>
           <Marquee pauseOnHover reverse className="[--duration:20s]">
-            {[1, 2, 3, 4].map((i) => (
-              <TestimonialCard key={i} quote="Some testimonial quote" author="John Doe" role="Patient" />
+            {data.map((item, i) => (
+              <TestimonialCard
+                key={i}
+                quote={item.quote}
+                author={item.author}
+                role={item.role}
+                avatarSrc={item.img}
+              />
             ))}
           </Marquee>
           <Marquee pauseOnHover className="[--duration:20s]">
-            {[1, 2, 3, 4].map((i) => (
-              <TestimonialCard key={i} quote="Some testimonial quote" author="John Doe" role="Patient" />
+            {data.map((item, i) => (
+              <TestimonialCard
+                key={i}
+                quote={item.quote}
+                author={item.author}
+                role={item.role}
+                avatarSrc={item.img}
+              />
             ))}
           </Marquee>
           <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
